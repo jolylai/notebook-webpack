d4bc67cb459e4bc173719a7c1359395186047897
jolylai <jolylai163@163.com>
2020-07-15 09:20:57 +0800

    feat: umirc

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'Webpack',
+  base: '/notebook-webpack',
+  publicPath: '/notebook-webpack/',
   favicon: 'https://cy-picgo.oss-cn-hangzhou.aliyuncs.com/webpack.svg',
   logo: 'https://cy-picgo.oss-cn-hangzhou.aliyuncs.com/webpack.svg',
   outputPath: 'docs-dist',
