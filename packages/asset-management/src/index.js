5e20fed3b241e0fec7c423f869c6424488f1811d
jolylai <jolylai163@163.com>
2022-05-06 18:03:05 +0800

    feat: assets

--- packages/asset-management/src/index.js
+++ packages/asset-management/src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import cat from '../asset/cat.png';
+
+(() => {
+  const img = document.createElement('img');
+  img.src = cat;
+  img.width = 180;
+  img.height = 180;
+  document.body.appendChild(img);
+})();
